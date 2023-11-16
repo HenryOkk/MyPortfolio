847c71d4131cef2412279861db3fed3f3b96cefc
Henry Ok <hwanok1020@gmail.com>
2023-11-15 23:43:16 -0600

    first commit

---
+++ src/Test.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const Test = () => {
+  return (
+    <div className="course">
+      <div className="box"></div>
+    </div>
+  );
+};
+
+export default Test;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import "./Navbar.scss";
+import { motion } from "framer-motion";
+import Sidebar from "../sidebar/Sidebar";
 // 15.5
 const Navbar = () => {
   return (
     <div className="navbar">
       {/* sidebar */}
+      <Sidebar />
       <div className="wrapper">
-        <span>Henry (Donghwan) Ok</span>
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          Henry (Donghwan) Ok
+        </motion.span>
         <div className="social">
           <a href="#">
             <img src="/facebook.png" alt="" />
